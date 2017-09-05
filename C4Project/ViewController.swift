344be2f62d5373701087780b8f5dc4d2904f25e9
椎木亮成 <ShinokiRyosei@zhuimuliangchengs-MacBook-Air.local>
2017-09-05 16:36:53 +0900

    Initial Commit

---
+++ C4Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  C4Project
+//
+//  Created by 椎木亮成 on 2017/09/05.
+//  Copyright © 2017 椎木亮成. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
